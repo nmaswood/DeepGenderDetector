0caac31de80ffec0700337e3725d9a85152a2d79
Nasr Maswood <nmaswood@uchicago.edu>
2017-09-10 21:24:00 -0500

    Started vectorize

--- process.py
+++ process.py
@@ -1,8 +1,13 @@
 from data import People
 
-class Process():
+class CharacterInformation():
 
-    data = People.read()
+    def __init__(self, data):
+        self.data = data
+        self.max_len = CharacterInformation._max_len(data)
+        self.all_chars= CharacterInformation._all_chars(data)
+        self.all_names = CharacterInformation._all_names(data)
+        self.char_indices, self.indices_char = CharacterInformation._create_dicts(self.all_chars)
 
     @staticmethod
     def _max_len(data):
@@ -31,6 +36,11 @@ class Process():
         return sorted(s)
 
     @staticmethod
+    def _all_names(data):
+
+        return data['name'].unique()
+
+    @staticmethod
     def _create_dicts(all_chars_sorted):
 
         char_indices = {}
@@ -44,16 +54,7 @@ class Process():
 
     @staticmethod
     def _vectorize(clean_data, vocab, max_word_len):
-        X = np.zeros((len(sentences), maxlen, len(chars)), dtype=np.bool)
-        y = np.zeros((len(sentences), len(chars)), dtype=np.bool)
-        for i, sentence in enumerate(sentences):
-            for t, char in enumerate(sentence):
-                X[i, t, char_indices[char]] = 1
-            y[i, char_indices[next_chars[i]]] = 1
-
-        # Reserve 0 for masking via pad_sequences
-
-        vocab_size = len(vocab) + 1
+        pdb.set_trace()
 
         word_idx = {c: i + 1 for i, c in enumerate(vocab)}
 
@@ -64,103 +65,36 @@ class Process():
 
         return X,y, word_idx
 
+class Vectorize:
+
+    def __init__(self, data, character_info):
+
+        self.data = data
+        self.character_info = character_info
+        self.X, self.y = Vectorize._vectorize(data, character_info)
+
     @staticmethod
-    def _vectorize_one(statement, vocab_dict, truthy, max_word_len):
+    def _vectorize(data,character_info):
 
-        """
+        X = np.zeros((l_data, max_charlen, all_charlen), dtype=np.bool)
+        y = np.zeros((l_data), dtype=np.int)
 
-        Converts one sentence and one truthy value into an x and y vector respectively
+        for index, row in data.iterrows():
 
-        """
+            name, male, female = row['name'], row['male'], row['female']
 
-        X = [vocab_dict[word] for word in statement]
+            gender_score = male - female
 
-        y = np.zeros(len(Truth.to_int))
-
-        y[truthy] = 1
+            for letter_index, letter in enumerate(name):
+                X[index, letter_index, character_info.char_indices[letter]] = 1
+            y[index] = gender_score
 
         return X,y
 
-    @staticmethod
-    def data_init(json_data):
-
-        """
-
-        Combines all the previous methods to clean and vectorize data
-
-        """
-
-        clean_data = Process._clean_data_and_split_statement(json_data)
-        all_words, max_word_len = Process._all_words(clean_data)
-        X,y, word_index = Process._vectorize(clean_data, all_words, max_word_len)
-
-        X = pad_sequences(X, maxlen = max_word_len),
-
-        return Data(
-                data = data,
-                clean_data = clean_data,
-                words = all_words,
-                word_index = word_index,
-                vocab_size = len(all_words) + 1,
-                max_word_len = max_word_len,
-                X = X,
-                y = y)
-
-
-class Data():
-
-    """
-
-    A struct to hold all the vectorized data information
-
-    """
-
-    def __init__(self,
-
-            data,
-            clean_data,
-            words,
-            word_index,
-            vocab_size,
-            max_word_len,
-            X,
-            y):
-        """
-        data
-            The original untouched data
-        clean_data
-            The cleaned data
-        word_index
-            mapping from words to indexes
-        vocab_size
-            Amount of words in vocab
-        max_word_len
-            The maximum length of a statement
-        X
-            The statements as vectors
-        y
-            The truth values as vectors
-        """
-
-        self.data = data
-        self.clean_data = clean_data
-        self.word_index = word_index
-        self.vocab_size = vocab_size
-        self.max_word_len = max_word_len
-        self.X = X
-        self.y = y
-
-    def __getitem__(self, key):
-
-        return getattr(self, key)
-
-
-
-
-
 if __name__ ==  "__main__":
-    x = Process()
-    d = Process.data
+    data = People.read()
+    character_info = CharacterInformation(data)
+    X,y = Vectorize(data, character_info)
 
-    res = Process._all_chars(d)
-    print (res)
+
+
